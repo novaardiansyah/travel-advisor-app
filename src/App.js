1b6f33611f671f2b6bc4e52c8c2498c55ef92c6a
novaardiansyah <novaardiansyah78@gmail.com>
2022-01-26 10:17:38 +0000

    create application directory structure
    - create the structure of the components
    - create dummy content
    - import all components into the App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// components
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
 const App = () => {
   return (
     <div>Hello World</div>
